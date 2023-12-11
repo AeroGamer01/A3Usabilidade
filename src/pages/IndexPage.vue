7c46649bb58c007c8066567d2759e1605977521f
AeroGamer01 <Lauro_carvalho@live.com>
2023-12-11 13:05:11 -0300

    git

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  {{ idx }}
+  <q-page class="flex flex-center">
+    <FaseCard :fase="idx" />
+   
+
+  </q-page>
+</template>
+
+<script>
+import FaseCard from 'src/components/FaseCard.vue';
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'IndexPage',
+    components: { FaseCard },
+})
+</script>
